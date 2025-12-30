efc597a48f8c526ef5a573477cb1a1b7e3a91e3f
KeyRayAdmin <crouch-throat-rush@duck.com>
2025-12-30 16:03:55 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const data = {
   movies: [
-    { title: "Avatar", year: 2009, poster: "https://via.placeholder.com/150", type: "Movie", link: "#" },
+    { title: "Avatar", year: 2009, poster: "https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg", type: "Movie", link: "#" },
     { title: "Inception", year: 2010, poster: "https://via.placeholder.com/150", type: "Movie", link: "#" }
   ],
   series: [
